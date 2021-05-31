f4e1f9db59afdd5b7789ba13fbc9a44e893c580d
Artem.Balov <Artem.Balov@urfu.me>
2021-05-31 18:07:30 +0500

    DictionarySaves

--- TechnicalEnglish/Form3.Designer.cs
+++ TechnicalEnglish/Form3.Designer.cs
@@ -32,11 +32,11 @@ namespace TechEng_0._2
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.button2 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -45,7 +45,7 @@ namespace TechEng_0._2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(130)))), ((int)(((byte)(109)))));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(175, 12);
+            this.button2.Location = new System.Drawing.Point(179, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(113, 49);
             this.button2.TabIndex = 3;
@@ -63,6 +63,22 @@ namespace TechEng_0._2
             this.panel2.TabIndex = 0;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(214, 433);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
@@ -73,41 +89,9 @@ namespace TechEng_0._2
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(130)))), ((int)(((byte)(109)))));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.textBox1.Location = new System.Drawing.Point(238, 72);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(130)))), ((int)(((byte)(109)))));
-            this.button1.Location = new System.Drawing.Point(68, 70);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(140, 25);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Добавить в словарь";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(214, 433);
-            this.richTextBox1.TabIndex = 8;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
             // richTextBox2
             // 
+            this.richTextBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
             this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox2.ForeColor = System.Drawing.SystemColors.Window;
@@ -117,6 +101,27 @@ namespace TechEng_0._2
             this.richTextBox2.TabIndex = 9;
             this.richTextBox2.Text = "";
             this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(130)))), ((int)(((byte)(109)))));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox1.Location = new System.Drawing.Point(241, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(130)))), ((int)(((byte)(109)))));
+            this.button1.Location = new System.Drawing.Point(15, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить в словарь";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form3
             // 
--- TechnicalEnglish/Form3.cs
+++ TechnicalEnglish/Form3.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,15 @@ namespace TechEng_0._2
 {
     public partial class Form3 : Form
     {
+        string pathText = Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Dictionary\\Words.txt");
+        string pathTranslatedText = Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Dictionary\\WordsTranslated.txt");
+
         public Form3()
         {
             InitializeComponent();
+
+            richTextBox1.Text = File.ReadAllText(pathText);
+            richTextBox2.Text = File.ReadAllText(pathTranslatedText);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -31,23 +38,36 @@ namespace TechEng_0._2
         private void textBox1_TextChanged(object sender, EventArgs e){}
         private void button1_Click_1(object sender, EventArgs e)
         {
+
             if (textBox1.Text == "")
             {
                 MessageBox.Show("Введите слово");
             }
                 else
             {
+                string translatedText = Translator.Translate(textBox1.Text, "en", "ru");
+
                 if (richTextBox1.Text == "")
                 {
                     richTextBox1.Text = (textBox1.Text);
-                    richTextBox2.Text = (Translator.Translate(textBox1.Text, "en", "ru"));
+                    richTextBox2.Text = (translatedText);
+
+                    File.AppendAllText(pathText, textBox1.Text + "\n");
+                    File.AppendAllText(pathTranslatedText,richTextBox2.Text + "\n");
+
                     textBox1.Text = "";
+
                 }
                 else
                 {
                     richTextBox1.Text = (richTextBox1.Text + "\n" + textBox1.Text);
-                    richTextBox2.Text = (richTextBox2.Text + "\n" + Translator.Translate(textBox1.Text, "en", "ru"));
+                    richTextBox2.Text = (richTextBox2.Text + "\n" + translatedText);
+
+                    File.AppendAllText(pathText, textBox1.Text + "\n");
+                    File.AppendAllText(pathTranslatedText,translatedText + "\n");
+
                     textBox1.Text = "";
+
 
                 }
             }
